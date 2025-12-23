bf9ae0c66ca498fb2ce4934fa122fceb0feeb353
danivjje <amdcollsisnt@gmail.com>
2025-12-23 23:42:01 +0200

    refactor(client/customers-table): refactor component

--- apps/client/src/components/CustomersTable.vue
+++ apps/client/src/components/CustomersTable.vue
@@ -31,7 +31,7 @@ const handleSort = async (data: DataTableSortEvent): Promise<void> => {
       sort.order = data.sortOrder === 1 ? 'asc' : 'desc';
     }
 
-    await customersStore.fetchCustomers(1, { ...sort } as SortParam);
+    await customersStore.fetchCustomers(1, sort.sort ? ({ ...sort } as SortParam) : undefined);
     customersStore.paginationFirstElement = 1;
   } catch (err) {}
 };
@@ -46,33 +46,38 @@ const handlePaginate = (page: number): void => {
 
 <template>
   <DataTable
-    :value="customersStore.customersData.customers"
-    :paginator="true"
-    :showGridlines="true"
-    :stripedRows="true"
-    :lazy="true"
-    :removableSort="true"
+    lazy
+    paginator
+    stripedRows
+    showGridlines
+    removableSort
     :rows="10"
-    :totalRecords="customersStore.customersData.total"
-    @sort="(data) => handleSort(data)"
     class="w-full"
     tableClass="min-w-125"
+    :value="customersStore.customersData.customers"
+    :totalRecords="customersStore.customersData.total"
+    @sort="(data) => handleSort(data)"
   >
-    <Column v-for="col of columnsData" :key="col.field" :field="col.field" :sortable="col.isSortable">
-      <template #header>
-        <span class="text-center font-medium">{{ col.header }}</span>
-      </template>
+    <Column
+      v-for="col of columnsData"
+      :key="col.field"
+      :field="col.field"
+      :header="col.header"
+      :sortable="col.isSortable"
+      :pt="{ columnHeaderContent: 'justify-center' }"
+    >
       <template #body="{ data }">
         <span class="block w-full text-center">{{ col.render(data[col.field]) }}</span>
       </template>
     </Column>
-    <template #paginatorcontainer="props">
+
+    <template #paginatorcontainer="{ totalRecords, rows }">
       <Paginator
-        :totalRecords="props.totalRecords"
-        :rows="props.rows"
+        :rows
+        :totalRecords
         :template="{
           '640px': 'PrevPageLink CurrentPageReport NextPageLink',
-          default: 'FirstPageLink PrevPageLink PageLinks NextPageLink LastPageLink',
+          default: 'FirstPageLink PrevPageLink CurrentPageReport NextPageLink LastPageLink',
         }"
         currentPageReportTemplate="{currentPage} из {totalPages}"
         @page="(page) => handlePaginate(page.page + 1)"
